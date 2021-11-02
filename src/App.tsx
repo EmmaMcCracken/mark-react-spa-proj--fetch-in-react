29d900fcba66f6b1780c7242e5e21430f27e8373
Emma <emma.mccracken14@gmail.com>
2021-11-02 16:24:37 +0000

    turned into dog app

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,36 @@
 import { useState } from "react";
 
-interface Joke {
-  id: number;
-  type: string;
-  setup: string;
-  punchline: string;
+// interface Joke {
+//   id: number;
+//   type: string;
+//   setup: string;
+//   punchline: string;
+// }
+interface DogImage {
+  message: string;
+  status: string;
 }
 
 function App() {
-  const [joke, setJoke] = useState<Joke>();
+  const [joke, setJoke] = useState<DogImage>();
 
   const handleGetJoke = async () => {
-    const response = await fetch(
-      "https://jokestemp.neillbogie.repl.co/jokes/general/random"
-    );
-    const jsonBody: Joke[] = await response.json();
-    setJoke(jsonBody[0]);
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const jsonBody: DogImage = await response.json();
+    setJoke(jsonBody);
   };
 
   // const handleGetJoke = () => {
-  //   fetch("https://jokestemp.neillbogie.repl.co/jokes/general/random")
+  //   fetch("https://dog.ceo/api/breeds/image/random")
   //     .then((response) => response.json())
-  //     .then((jsonBody: Joke[]) => setJoke(jsonBody[0]));
+  //     .then((jsonBody: DogImage[]) => setJoke(jsonBody[0]));
   // };
 
   if (joke) {
     return (
       <div>
-        <h1>Joke app</h1>
-        <details>
-          <summary>{joke.setup}</summary>
-          <p>{joke.punchline}</p>
-        </details>
-        <hr />
-        <button onClick={handleGetJoke}>Get another joke</button>
+        <img src={joke.message} alt="hi" />
+        <button onClick={handleGetJoke}>Get another dog photo</button>
       </div>
     );
   } else {
@@ -42,8 +39,9 @@ function App() {
         <h1>Joke app</h1>
         <p>
           Click the button to trigger a <code>fetch</code> that gets a random
-          joke from an API!
+          dog photo from an API!
         </p>
+
         <button onClick={handleGetJoke}>Get joke</button>
       </div>
     );
